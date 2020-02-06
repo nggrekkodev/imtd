2b3ea745e0fe8f1fd899824c7a113308fd56c8d0
nggrekkodev <nggrekkodev@gmail.com>
2020-02-06 15:41:20 +0100

    update

--- sandbox-angular/src/app/app-routing.module.ts
+++ sandbox-angular/src/app/app-routing.module.ts
@@ -1,11 +1,15 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { PageNotFoundComponent } from "./components/page-not-found/page-not-found.component";
+import { LeafletMapComponent } from "./components/leaflet-map/leaflet-map.component";
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", component: LeafletMapComponent },
+  { path: "**", component: PageNotFoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- sandbox-angular/src/app/app.module.ts
+++ sandbox-angular/src/app/app.module.ts
@@ -6,9 +6,10 @@ import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { NavbarComponent } from "./components/navbar/navbar.component";
 import { LeafletMapComponent } from "./components/leaflet-map/leaflet-map.component";
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 
 @NgModule({
-  declarations: [AppComponent, NavbarComponent, LeafletMapComponent],
+  declarations: [AppComponent, NavbarComponent, LeafletMapComponent, PageNotFoundComponent],
   imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent]
